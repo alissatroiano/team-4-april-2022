cdf957c141285beadebb33d7be4f504d5966b404
Darlyn Norlay <norlaydee@hotmail.co.uk>
2022-04-23 01:06:34 +0100

    Added onclick id recongistion for car size and budget questions

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+//Car size question
+let carSize = ""  // define variables to store data
+// function for car type question - make one for each question
+function selectCarSize(value) {
+    let carSize = value
+    console.log(carSize)
+} 
+
+
+//Budget question
+let budgetSize = ""  // define variables to store data
+// function for car type question - make one for each question
+function selectBudgetSize(value) {
+    let budgetSize = value
+    console.log(budgetSize)
+} 
