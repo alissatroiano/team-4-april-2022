f57fb3a1a81bbbd48916d6a5f46658df12dc94e8
Darlyn Norlay <norlaydee@hotmail.co.uk>
2022-04-23 01:43:38 +0100

    Added selected option border code and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
 
+function selectedCardCSS (value) {
+    // find the selected element
+    selection = document.getElementById(value)
+    // find the parent element containing all the options
+    let container = selection.parentElement.parentElement
+    // select the cards of the options
+    options = container.getElementsByClassName("card")
+    // update css
+    for (let i = 0; i < options.length; i++){
+        if (options[i].id == value) {
+            selection.classList.add("card-selected")
+        } else {
+            options[i].classList.remove("card-selected")
+        }
+    }
+}
+
+
+
 //Car size question
 let carSize = ""  // define variables to store data
 // function for car type question - make one for each question
 function selectCarSize(value) {
     let carSize = value
     console.log(carSize)
+    selectedCardCSS(value)
 } 
 
 
@@ -14,6 +34,7 @@ let budgetSize = ""  // define variables to store data
 function selectBudgetSize(value) {
     let budgetSize = value
     console.log(budgetSize)
+    selectedCardCSS(value)
 } 
 
 
@@ -23,4 +44,9 @@ let mileageSize = ""  // define variables to store data
 function selectMileageSize(value) {
     let mileageSize = value
     console.log(mileageSize)
+    selectedCardCSS(value)
 } 
+
+
+
+
