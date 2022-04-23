e9ee6af3b199fd0ab64bf5e2d3d95806fd9e7269
Alissa Troiano <alissatroianonyc@gmail.com>
2022-04-22 21:39:40 -0400

    Move mission statement content to homepage and clean up layout

--- assets/js/create-nav-and-footer.js
+++ assets/js/create-nav-and-footer.js
@@ -14,15 +14,14 @@ function createNavbar () {
               <a class="nav-link active" aria-current="page" href="index.html">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link font-dark" href="mission.html">Mission</a>
+              <a class="nav-link font-dark" href="#mission">Mission</a>
             </li>
+            <li class="nav-item" id="cta-nav">
+            <a class="nav-link" href="questions.html">EValuator</a>
+          </li>
             <li class="nav-item">
               <a class="nav-link" href="team.html">Team</a>
             </li>
-            <li class="nav-item" id="cta-nav">
-              <a class="nav-link" href="questions.html">Start Here</a>
-            </li>
-
           </ul>
         </div>
       </div>
@@ -40,16 +39,16 @@ function createFooter () {
       <div class="col-4 justify-content-center d-flex">
         <ul class="list-group list-unstyled list-group-horizontal-md" id="footer-links">
           <li class="ms-3"><a class="text-muted" href="index.html">Home</a></li>
-          <li class="ms-3"><a class="text-muted" href="mission.html">Our Mission</a></li>
+          <li class="ms-3"><a class="text-muted" href="#mission">Our Mission</a></li>
           <li class="ms-3"><a class="text-muted" href="team.html">Our Team</a></li>
           <li class="ms-3" id="footer-cta"><a class="footer-link" href="questions.html">Start Here</a></li>
         </ul>
       </div>
       <div class="col-4 justify-content-center d-flex">
         <ul class="nav list-unstyled">
-          <li class="ms-3"><a class="text-muted" href="#"><i class="fab fa-twitter"></i></a></li>
-          <li class="ms-3"><a class="text-muted" href="#"></a><i class="fab fa-facebook"></i></li>
-          <li class="ms-3"><a class="text-muted" href="#"></a><i class="fab fa-linkedin"></i></li>
+          <li class="mx-3"><a class="text-muted" href="#"><i class="fab fa-twitter"></i></a></li>
+          <li class="mx-3"><a class="text-muted" href="#"></a><i class="fab fa-facebook"></i></li>
+          <li class="mx-3"><a class="text-muted" href="#"></a><i class="fab fa-linkedin"></i></li>
         </ul>
       </div>
     </footer>
