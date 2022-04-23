4e11b9f4d84c5fa82d8a18e23622d6a53b078429
Darlyn Norlay <norlaydee@hotmail.co.uk>
2022-04-23 01:15:15 +0100

    Added onclick id recgonition for mileage question

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,12 @@ function selectBudgetSize(value) {
     let budgetSize = value
     console.log(budgetSize)
 } 
+
+
+//Mileage question
+let mileageSize = ""  // define variables to store data
+// function for car type question - make one for each question
+function selectMileageSize(value) {
+    let mileageSize = value
+    console.log(mileageSize)
+} 
