af94c922b70116509eb1de77a229123643dde997
nlenno1 <nick.lennon@hotmail.co.uk>
2022-04-24 14:27:58 +0000

    feat: add price filtering to car search, limit results to 2 and shuffle results

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,52 +16,56 @@ function selectedCardCSS (value) {
     }
 }
 
-// Car questions
+// Car Search  ---------------------------------------------------- //
+
 // Global Variables
 var carSizeInput = null;
 var budgetInput = null;
 
-// functions to set local variables on click
+// function to set local variables on click
 function setOptionVariable(value) {
     if (value.substring(0,2) == "Q1") {
         carSizeInput = value;
     } else if (value.substring(0,2) == "Q2") {
         budgetInput = value;
     }
-    console.log(carSizeInput)
-    console.log(budgetInput)
     selectedCardCSS(value);
 }
 
-function selectMileageSize(value) {
-    let mileageSize = value;
-    console.log(mileageSize);
-    selectedCardCSS(value);
-}
-
+// function to display the car search results
 function displaySearchResults(resultsList) {
+    // find the display element and clear it
     display = document.getElementById("searchResultsDisplay")
     display.innerHTML = ""
-    for (const car of resultsList) {
-        display.innerHTML += `
-        <div class="card car-display-card">
-            <div class="card-header">
-              <h5 class="card-title">${car['MANUFACTURER']} - ${car['MODEL']}</h5>
+    if (resultsList.length >= 2) {
+        // more than 2 results in the list, shuffle the list and take the first 2 results
+        if (resultsList.length > 2){
+            resultsList = resultsList.sort(() => Math.random() - 0.5)
+            var resultsList = resultsList.slice(0,2)
+        }
+        // for all the items in the results, display thier info on screen
+        for (const car of resultsList) {
+            display.innerHTML += `
+            <div class="card car-display-card">
+                <div class="card-header">
+                  <h5 class="card-title">${car['MANUFACTURER']} - ${car['MODEL']}</h5>
+                </div>
+                <div class="card-body py-5">
+                  <img src="${car['img']}" alt="Image of a ${car['MANUFACTURER']} ${car['MODEL']}">
+                  <h5>Specification</h5>
+                  <p class="mb-0">Engine Power - ${car['ENGINE POWER']}</p>
+                  <p class="mb-0">Battery Capacity - ${car['BATTERY CAPACITY']}</p>
+                  <p class="mb-0">Miles Per Kilo Watt Hour - ${car['m/kWh']}</p>
+                  <p class="mb-0">Price - $${car['PRICE']}</p>
+                </div>
             </div>
-            <div class="card-body py-5">
-              <img src="${car['img']}" alt="Image of a ${car['MANUFACTURER']} ${car['MODEL']}">
-              <h5>Specification</h5>
-              <p class="mb-0">Engine Power - ${car['ENGINE POWER']}</p>
-              <p class="mb-0">Battery Capacity - ${car['BATTERY CAPACITY']}</p>
-              <p class="mb-0">Miles Per Kilo Watt Hour - ${car['m/kWh']}</p>
-              <p class="mb-0">Price - $${car['PRICE']}</p>
-            </div>
-        </div>
-        `
+            `
+        }
     }
 }
 
 function searchButtonClick() {
+    // validate user input with an alert if options are not clicked
     if (budgetInput == null && carSizeInput == null) {
         alert("please select your car budget above and prefered car size above")
         return false;
@@ -72,36 +76,48 @@ function searchButtonClick() {
         alert("please select your car budget above")
         return false;
     } else {
-        console.log("options selected")
-        console.log(carSizeInput)
-        console.log(budgetInput)
-        var searchResults = [];
+        // collect data from the local database
         fetch("../../cars/fixtures/Electric.json")
         .then(
             response => response.json()
         )
         .then(
             carData => {
+                // set empty storage lists
+                var resultsStorage = [];
+                var searchResults = [];
+                // filer all vehicles by vehicle class/size
                 for (let i = 0; i < carData.length; i++) {
                     if (carData[i]["VEHICLE CLASS"].toLowerCase() == carSizeInput.slice(3)) {
-                        searchResults.push(carData[i])
+                        resultsStorage.push(carData[i])
                     }
                 }
+                // filer all vehicles by price in brackets
+                switch (budgetInput.slice(3)) {
+                    case "Under30":
+                        for (let i = 0; i < resultsStorage.length; i++) {
+                            if (resultsStorage[i]["data_price"] < 30000) {
+                                searchResults.push(resultsStorage[i])
+                            }
+                        }
+                        break;
+                    case "30To50":
+                        for (let i = 0; i < resultsStorage.length; i++) {
+                            if (resultsStorage[i]["data_price"] >= 30000 && resultsStorage[i]["data_price"] <= 50000) {
+                                searchResults.push(resultsStorage[i])
+                            }
+                        }
+                        break;
+                    case "Over50":
+                        for (let i = 0; i < resultsStorage.length; i++) {
+                            if (resultsStorage[i]["data_price"] > 50000) {
+                                searchResults.push(resultsStorage[i])
+                            }
+                        }
+                        break;
+                    }
+                // call the display search results function
                 displaySearchResults(searchResults)
-                // let suggestions = carData.map(suggestions => [
-                //     suggestions.img,
-                //     suggestions.MODEL,
-                //     suggestions.MANUFACTURER,
-                //     suggestions.VEHICLE_CLASS,
-                //     suggestions.ENGINE_POWER,
-                //     suggestions.BATTERY_CAPACITY,
-                //     suggestions.PRICE
-                // ]);
-                // suggestions.forEach((suggestions => {
-                //     console.log(suggestions[3],[7])
-                // }));
-                // console.log(suggestions);
             }
-        );
-        }
+        )}
     }
